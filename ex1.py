ba2492db79f9d48987a759b38dc21c9d76c5ba03
genius-devops <adamslabaran@gmail.com>
2024-01-22 14:29:24 +0100

    monday commit

---
+++ ex1.py
@@ -0,0 +1,32 @@
+Patricia operates a grocery store selling several product categories as shown below.
+1. Foods
+2. Beverages
+3. Toiletries
+She estimates that the total sales each week for Foods is $3210. 
+The daily workday sales of beverages are $520. On weekends (Saturdays and Sundays) 
+the daily sales are double the weekday sale for this category.
+The daily sales for toiletries are about 83% of the beverages and shows the same pattern for weekdays and weekends.
+Patricia employe 5 persons as accounting and sales staff with a 1,500 monthly salary per staff. She is also considering paying herself a monthly salary of $2750.
+
+Patricia's profit margin on Foods is 25.30%, on Beverages its 15.76% and on Toiletries its 35% lower than that of Beverages.
+Costs associated with her Grocery store are monthly rent of 2100, monthly cost of logistics for purchase and restocking inventory is 5% of total monthly revenue from the store.
+Tax is charged at 25.3% of profit. There is also a 5% VAT on Toiletries and Beverages that is already included in the selling price of products in these categories.
+Patricia would like to disburse her profit after tax and all deductions into the following three account bucket such that investment in inventory gets a minimum of $ 20,000 per month;
+
+1. 40% remitted to her investment account which yields 15% interest per annum
+2. 30% held in a current account for domestic expenses
+3. 30% invested back into inventory in the Grocery store
+
+TASK
+1. compute the total revenue and profits per category
+2. compute the total tax and vat payable on each category
+3. compute total store expenses on a monthly and annual basis
+4. what percentage of profits is employee salary
+5. If Patricia pays herself the proposed salary, by what percentage does profitability drop monthly
+6. Compute the amount of money remitted into the three account buckets every month
+
+
+#solution
+a = 3
+print(a)
+
